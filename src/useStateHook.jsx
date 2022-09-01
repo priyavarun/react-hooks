2650e1a91edc328afe66b6ac5b5f592fbb1ce6e9
Priya <priyyavarun@gmail.com>
2022-09-01 12:15:03 +0530

    first commit

---
+++ src/useStateHook.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const useStateHookExample = () => {
+  const [count, setCount] = useState(() => {
+    console.log("This function is run only once");
+
+    // Its run only on first render. Post that React maintains the state of the count internally from useState hook and re-renders.
+    return 10;
+  });
+
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrementCount = () => {
+    setCount((prevCount) => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>Current count: {count}</h1>
+      <h2>Increment count++: <button onClick={incrementCount}>Increment</button></h2>
+      <h2>Decrement count--: <button onClick={decrementCount}>Increment</button></h2>
+    </div>
+  );
+};
+
+export default useStateHookExample;
